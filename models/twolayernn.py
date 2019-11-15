f80c54e9ba86417ef6cfa64c5067c0fd852007d4
DivyanshRoy <divyanshkumarroy@gmail.com>
2019-11-15 17:32:37 -0500

    Pushed One and Two Layer NN

---
+++ models/twolayernn.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TwoLayer(nn.Module):
+    def __init__(self, im_size, n_classes):
+        """ OneLayer Classifier
+
+        Arguments:
+            im_size (tuple): A tuple of ints with (channels, height, width)
+            n_classes (int): Number of classes to score
+        """
+        super(TwoLayer, self).__init__()
+
+        flattened_input_dim = im_size[0] * im_size[1] * im_size[2]
+        self.layer1 = nn.Linear(flattened_input_dim, 512)
+        self.layer2 = nn.Linear(512, 64)
+        self.layer3 = nn.Linear(64, n_classes)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, images):
+        """
+        Arguments:
+            images (Variable): A tensor of size (N, C, H, W) where
+                N is the batch size
+                C is the number of channels
+                H is the image height
+                W is the image width
+
+        Returns:
+            A torch Variable of size (N, n_classes) specifying the score
+            for each example and category.
+        """
+        flattened_images = images.reshape(images.shape[0], -1)
+        x = F.relu(self.layer1(flattened_images))
+        x = F.relu(self.layer2(x))
+        unnormalised_scores = self.layer3(x)
+        logits = self.softmax(unnormalised_scores)
+        return logits, unnormalised_scores
---
+++ scripts/run_twolayernn.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source activate img-classifcation
+
+flags="--model twolayernn \
+       --data-aug \
+       --optimiser adam \
+       --learning-rate 0.001 \
+       --lr-reducer \
+       --lr-lambda-scheduler \
+       --epochs 80 \
+       --batch-size 64 \
+       --train-data-size 49000\
+       --exp-name resnet \
+       --tensorboard \
+       --filelogger "
+
+unbuffer python train.py $flags | tee checkpoints/twolayernn.log
