30fb484d6e0d5ef2eae0ce3680f5011f23f352d2
Amal Syriac <asyriac3@gatech.edu>
2019-11-16 11:52:32 -0500

    Changed num epochs for VGGNet

--- scripts/run_vggnet.sh
+++ scripts/run_vggnet.sh
@@ -9,7 +9,7 @@ flags="--model vggnet \
        --learning-rate 0.01 \
        --momentum 0.9 \
        --weight-decay 5e-4 \
-       --epochs 350 \
+       --epochs 200 \
        --batch-size 128 \
        --test-batch-size 128 \
        --data-aug \
